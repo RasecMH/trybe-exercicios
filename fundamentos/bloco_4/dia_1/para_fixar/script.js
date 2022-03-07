32b910d0a03bf04afdd3211209ffe297848df17c
cesar <rasecmh0@gmail.com>
2022-03-07 15:29:52 -0300

    Parte 1 de para fixar

---
+++ fundamentos/bloco_4/dia_1/para_fixar/script.js
@@ -0,0 +1,14 @@
+
+// Criação de constantes e variáveis
+const myName = "Cesar";
+const birthCity = "Fortaleza";
+let birthYear = 1995;
+
+// Alteração de atributos das variáveis e constantes
+birthYear = 2030;
+//birthCity = "Recife";
+
+// Console.log para printar conteúdo das varáveis e constantes na tela
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
