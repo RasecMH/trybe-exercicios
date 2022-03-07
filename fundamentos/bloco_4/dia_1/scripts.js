16663c497f10374b86e3d0ec39947fc20912f9b8
cesar <rasecmh0@gmail.com>
2022-03-07 19:20:16 -0300

    Adiciona retornar maior valor

---
+++ fundamentos/bloco_4/dia_1/scripts.js
@@ -0,0 +1,33 @@
+//Constantes
+const a = 10;
+const b = 20;
+const c = 15;
+
+//Programas
+
+//Adição
+const soma = a + b;
+console.log("Resultado da soma é:", soma);
+
+//Subtração
+const subtracao = a - b;
+console.log("Resultado da subtração é:", subtracao);
+
+//Multiplicação
+const multiplicacao = a * b;
+console.log("Resultado da multiplicação é:", multiplicacao);
+
+//Divisão
+const divisao = a / b;
+console.log("Resultado da divisão é:", divisao);
+
+//Módulo
+const resto = a % b;
+console.log("Resultado do resto é:", resto);
+
+//Retornar o maior entre dois números
+if(a > b == true) {
+  console.log(a);
+}else {
+  console.log(b);
+}
