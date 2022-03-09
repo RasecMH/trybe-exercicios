3b28da7f06b15aa3d3bf98b81e74f4f8df68c526
cesar <rasecmh0@gmail.com>
2022-03-09 19:28:56 -0300

    Parte 2 do primeiro exercicio

---
+++ fundamentos/bloco_4/dia_2/exercicio-1.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+// Imprimir os valores contidos no array
+for (const numeros of numbers) {
+  console.log(numeros);
+}
+
+// Somar os valores contidos no array
+for (let index = 0; index < numbers.length; index++) {
+  soma += numbers[index];
+}
+
+console.log(soma);
