6fca473bd6cf290c28b3287a5e3b18639b87a34f
cesar <rasecmh0@gmail.com>
2022-03-09 19:53:56 -0300

    Parte 7 do exercicio

--- fundamentos/bloco_4/dia_2/exercicio-1.js
+++ fundamentos/bloco_4/dia_2/exercicio-1.js
@@ -22,27 +22,38 @@ console.log('A média aritmética é:', mediaAri);
 //Caso a média aritmética seja maior que 20 imprimir
 if (mediaAri > 20) {
   console.log('Valor acima é maior que 20');
-}else {
+} else {
   console.log('Valor acima é menor que 20');
 }
 
 //Verificar o maior valor contido no array
 let maiorNumero = numbers[0];
-for (let i2 = 1; i2 < numbers.length; i2+=1) {
-  if(maiorNumero < numbers[i2]){
+for (let i2 = 1; i2 < numbers.length; i2 += 1) {
+  if (maiorNumero < numbers[i2]) {
     maiorNumero = numbers[i2]
   }
-  
+
 }
 console.log('O maior número do array é:', maiorNumero)
 
 // Descobrir quantos impares tem no array
 let totalImpares = 0;
 
-for(let i3 = 0; i3 < numbers.length; i3+=1){
-  if(numbers[i3] % 2 != 0){
+for (let i3 = 0; i3 < numbers.length; i3 += 1) {
+  if (numbers[i3] % 2 != 0) {
     totalImpares += 1;
   }
 }
 
 console.log('O total de números impares é:', totalImpares);
+
+// Verificar o menor número
+let menorNumero = numbers[0];
+for (let i4 = 1; i4 < numbers.length; i4 += 1) {
+  if (menorNumero > numbers[i4]) {
+    menorNumero = numbers[i4]
+  }
+
+}
+console.log('O menor número do array é:', menorNumero)
+
