164e26f593a95b512d088e9e3273b05c68557922
cesar <rasecmh0@gmail.com>
2022-03-09 05:03:49 -0300

    Calculo de salario

--- fundamentos/bloco_4/dia_1/scripts.js
+++ fundamentos/bloco_4/dia_1/scripts.js
@@ -128,3 +128,37 @@ if(a % 2 > 0 || b % 2 > 0 || c % 2 > 0){
 /*//Calcular lucro de produtos
 console.log("O lucro da venda será de:", lucro * quantidadeProdutos);
 */
+
+//Calculo de salário bruto
+
+let aliquotaInss;
+let aliquotaIr;
+let salarioBruto = 3000.00;
+
+if(salarioBruto <= 1556.94){
+  aliquotaInss = salarioBruto * 0.08;
+}else if(salarioBruto <= 2594.92){
+  aliquotaInss = salarioBruto * 0.09;
+}else if(salarioBruto <= 5189.82){
+  aliquotaInss = salarioBruto * 0.11;
+}else{
+  aliquotaInss = 570.88;
+}
+
+const salarioBase = salarioBruto - aliquotaInss;
+
+if(salarioBase <= 1903.98){
+  aliquotaIr = 0;
+}else if(salarioBase <= 2826.65){
+  aliquotaIr = (salarioBase * 0.075) - 142.80;
+}else if(salarioBase <= 3751.05){
+  aliquotaIr = (salarioBase * 0.15) - 354.80;
+}else if(salarioBase <= 4664.68){
+  aliquotaIr = (salarioBase * 0.225) - 636.13
+}else{
+  aliquotaIr = (salarioBase * 0.275) - 869.36;
+}
+
+const salarioLiquido = salarioBase - aliquotaIr;
+
+console.log(salarioLiquido);
