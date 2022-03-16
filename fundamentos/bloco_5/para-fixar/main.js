0e4bbc788791e3acd8505c4dd6a8175cef841412
cesar <rasecmh0@gmail.com>
2022-03-16 19:27:54 -0300

    Adicionar change ao input

--- fundamentos/bloco_5/para-fixar/main.js
+++ fundamentos/bloco_5/para-fixar/main.js
@@ -24,10 +24,17 @@ thirdLi.addEventListener('click', selecionado);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+function textoDigitado(event){
+  techAtual = document.getElementsByClassName('tech');
+  techAtual[0].innerText = input.value;
+}
 
+input.addEventListener('change', textoDigitado);
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+
+
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
