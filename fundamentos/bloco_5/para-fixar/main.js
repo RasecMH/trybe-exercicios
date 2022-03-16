67df26dc7c41f88272bdefc0231453ab82b7602b
cesar <rasecmh0@gmail.com>
2022-03-16 19:40:01 -0300

    Adiciona um link ao myWebpage

--- fundamentos/bloco_5/para-fixar/main.js
+++ fundamentos/bloco_5/para-fixar/main.js
@@ -34,7 +34,11 @@ input.addEventListener('change', textoDigitado);
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
+function cliqueDuplo(event){
+  window.open("https://rasecmh.github.io")
+}
 
+myWebpage.addEventListener('dblclick', cliqueDuplo);
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
